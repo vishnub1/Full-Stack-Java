e11f99c6036676087263ec3c9e44f28ee0c9cb5a
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-03-09 17:14:01 +0530

    Create Ass1.java

---
+++ Core Java/Ass1.java
@@ -0,0 +1,27 @@
+
+public class Ass1 {
+	public static void main(String [] args) {
+		int a=10,b=20,c=5,d;
+		d = a+b-c;
+		
+		int x=200, y=3,z;
+		z=x*y;
+		
+		int p=40,q=10,r;
+		r=p*q;
+		
+		System.out.println("A :"+a);
+		System.out.println("B :"+b);
+		System.out.println("C :"+c);
+		System.out.println("Result :"+d);
+		
+		System.out.println("\nx :"+x);
+		System.out.println("y :"+y);
+		System.out.println("Result :"+z);
+		
+		System.out.println("\np :"+p);
+		System.out.println("q :"+q);
+		System.out.println("Result :"+r);
+	}
+}
+		
