f1f97216fbe72a7bcffb391290f04fa3c5575a5e
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-03-24 22:43:19 +0530

    Create Address.java

---
+++ Core Java/References/Address.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Address {
+	public String hNo,sName,city;
+	public int pinCode;
+	public void getAddress() {
+	System.out.println("===Employee Address===");
+	System.out.println("HNo\t :"+hNo);
+	System.out.println("SName\t :"+sName);
+	System.out.println("City\t :"+city);
+	System.out.println("pincode\t :"+pinCode);
+	}
+}
