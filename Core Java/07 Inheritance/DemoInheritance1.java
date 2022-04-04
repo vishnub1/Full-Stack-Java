f1b62f4c3ab4fac71ba83ce2da9b125027dde1cc
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-04-04 21:40:52 +0530

    Update DemoInheritance1.java

--- Core Java/07 Inheritance/DemoInheritance1.java
+++ Core Java/07 Inheritance/DemoInheritance1.java
@@ -1,1 +1,42 @@
-a
+//PClass.java
+package test;
+public class PClass {
+	public int a=10;
+	public void m1()
+	{
+		System.out.println("==PClass M1()==");
+		System.out.println("Value of a :"+a);		
+	}
+	{
+		System.out.println("==PClass Instance block==");
+		System.out.println("The value of a :"+a);
+	}
+}
+
+//PClass.java
+package test;
+public class CClass extends PClass {
+	public int b=20;
+	public void m2()
+	{
+		System.out.println("==CClass m2()==");
+		System.out.println("The value of b:"+b);
+	}
+	{
+		System.out.println("CClass Instance Block==");
+		System.out.println("The value of b :"+b);
+	}
+}
+
+
+//Case-1 : NonStatic members from the ParentClass/SuperClass
+//Inheritance_Ex1.java
+package maccess;
+import test.*;
+public class Inheritance_Ex1 {
+	public static void main(String[] args) {
+		CClass ob = new CClass();
+		ob.m1();
+		ob.m2();
+	}
+}
