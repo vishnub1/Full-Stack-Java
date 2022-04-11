3315f97ee4cc1a8ef0dd00c4aa33d61e50a6c004
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-04-11 22:05:08 +0530

    Create DBCon1.java

---
+++ JDBC/DBCon1.java
@@ -0,0 +1,20 @@
+package test;
+import java.sql.*;
+public class DBCon1 {
+	public static void main(String[] args) {
+        try {
+        	Class.forName("oracle.jdbc.driver.OracleDriver");      //step-1
+        	Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","manager"); //step-2
+        	Statement stm = con.createStatement();                 //step-3
+        	ResultSet rs = stm.executeQuery("select * from Employee44");//step-4
+        	while(rs.next())
+          {
+        		System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+
+        	         rs.getString(3)+"\t"+rs.getInt(4));
+        	}
+        	con.close();    //step-5
+        }catch(Exception e) {
+          e.printStackTrace();
+      }
+	}
+}
