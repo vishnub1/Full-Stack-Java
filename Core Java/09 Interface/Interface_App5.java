214e37450d20332119f9d569ee7cd9b02bbe190e
Vishnu Biradar <91277493+vishnub1@users.noreply.github.com>
2022-04-11 22:13:46 +0530

    Update Interface_App5.java

--- Core Java/09 Interface/Interface_App5.java
+++ Core Java/09 Interface/Interface_App5.java
@@ -46,3 +46,49 @@ public class ModuloDivision {
 		System.out.println("ModDiv :"+(x%y));
 	}
 }
+
+
+//package :: maccess
+// DemoInterface5.java
+
+package maccess;
+import test.*;
+import java.util.*;
+public class DemoInterface5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value 1");
+		int v1 = sc.nextInt();
+		System.out.println("Enter the value 2");
+		int v2 = sc.nextInt();
+		System.out.println("===choice===");
+		System.out.println("1.Add\n2.Sub\n3.Mul\n4.Div\n5.ModDiv");
+		System.out.println("Enter the choice");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			Addition ad = new Addition();
+			ad.calculate(v1, v2);
+			break;
+		case 2:
+			Substraction sb = new Substraction();
+			sb.calculate(v1, v2);
+			break;
+		case 3:
+			Multiplication mp = new Multiplication();
+			mp.calculate(v1, v2);
+			break;
+		case 4:
+			Division dv = new Division();
+			dv.calculate(v1, v2);
+			break;
+		case 5:
+			ModuloDivision md = new ModuloDivision();
+			md.calculate(v1, v2);
+			break;
+		default:
+			System.out.println("Invalid choice 🙄🤦‍♂️");
+		}
+	}
+}
+
